20dec8c2953fd4daf2c79b89e2d4a540e717e015
Dave Goehrig <dave@dloh.org>
2016-05-03 12:22:04 -0400

    working relay

--- src/urelay_room.erl
+++ src/urelay_room.erl
@@ -3,14 +3,16 @@
 -copyright(<<"(C) 2016 David J. Goehrig"/utf8>>).
 -behavior(gen_server).
 -export([ start_link/2, 
-	join/2, leave/2, ban/2,
-	announce/2, message/3, broadcast/2, 
-	list/1, close/1 ]).
+	join/2, leave/2, ban/2, unban/2, peer/2,
+	message/3, broadcast/2, 
+	users/1, bans/1, peers/1,
+	close/1 ]).
 -export([ code_change/3, handle_call/3, handle_cast/2, handle_info/2, init/1,
 	terminate/2 ]).
 
--record(user_id, { ipaddr, port, name }).
--record(room, { name, users, bans }).
+-record(peer, { ipaddr, port }).
+-record(user, { ipaddr, port, name }).
+-record(room, { name, users, bans, peers, socket, port }).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Public API
@@ -27,8 +29,11 @@ leave(Room,Id) ->
 ban(Room,Id) ->
 	gen_server:call(Room, { ban, Id } ).
 
-announce(Room,Id) ->
-	gen_server:call(Room, { announce, Id }).
+unban(Room,Id) ->
+	gen_server:call(Room, { unban, Id } ).
+
+peer(Room,Peer) ->
+	gen_server:call(Room, { peer, Peer }).
 
 message(Room,Id,Message) ->
 	gen_server:call(Room, { message, Id, Message }).
@@ -36,8 +41,14 @@ message(Room,Id,Message) ->
 broadcast(Room,Message) ->
 	gen_server:call(Room, { broadcast, Message }).
 
-list(Room) ->
-	gen_server:call(Room, list).
+users(Room) ->
+	gen_server:call(Room, users).
+
+bans(Room) ->
+	gen_server:call(Room, bans).
+
+peers(Room) ->
+	gen_server:call(Room, peers).
 
 close(Room) ->
 	gen_server:call(Room, close).
@@ -48,8 +59,10 @@ close(Room) ->
 %
 
 init([ Name, Port ]) ->
-	io:format("Starting room ~p~n", [ Name ]),
-	{ ok, #room{ name = Name, users = [], bans = [] }}.
+	io:format("Starting room ~p on port ~p~n", [ Name, Port ]),
+	{ ok, Socket } = gen_udp:open(Port, [ binary, { active, true }]),
+	{ ok, #room{ name = Name, users = [], bans = [], peers = [], 
+		port = Port, socket = Socket }}.
 
 handle_call( { join, Id }, _From, Room = #room{ users = Users, bans = Bans }) ->
 	case lists:member( Id, Bans ) of
@@ -60,27 +73,41 @@ handle_call( { join, Id }, _From, Room = #room{ users = Users, bans = Bans }) ->
 	end;
 
 handle_call( { leave, Id }, _From, Room = #room{ users = Users }) ->
-	{ reply, ok, Room#room{ users = lists:delete(Id,Users) }};
+	{ reply, ok, Room#room{ users = users:delete(Id,Users) }};
 
 handle_call( { ban, Id }, _From, Room = #room{ users = Users, bans = Bans }) ->
 	{ reply, ok, Room#room{ 
 		users = lists:delete(Id,Users),
 		bans = [ Id | Bans ] }};
 
-handle_call( { announce, Id }, _From, Room = #room{ users = Users }) ->
+handle_call( { unban, Id }, _From, Room = #room{ bans = Bans }) ->
+	{ reply, ok, Room#room{ bans = lists:delete(Id,Bans) }};
+
+handle_call( { peer, Peer = #peer{} }, _From, Room = #room{ peers = Peers }) ->
+	{ reply, ok, Room#room{ peers = [ Peer | Peers ] }};
+
+handle_call( { message, #user{ ipaddr = IPAddr, port = Port }, Message }, _From, Room = #room{ socket = Socket }) ->
+	gen_udp:send(Socket,IPAddr,Port,Message),
 	{ reply, ok, Room };
 
-handle_call( { message, Id, Message }, _From, Room = #room{ users = Users }) ->
+handle_call( { broadcast, Message }, _From, Room = #room{ 
+	users = Users, peers = Peers, socket = Socket }) ->
+	[ broadcast(Socket,U,Message) || U <- Users ],
+	[ relay(Socket,P,Message) || P <- Peers ],
 	{ reply, ok, Room };
 
-handle_call( { broadcast, Message }, _From, Room = #room{ users = Users }) ->
-	{ reply, ok, Room };
+handle_call( users, _From, Room = #room{ users = Users }) ->
+	{ reply, { ok, Users }, Room };
 
-handle_call( list, _From, Room = #room{ users = Users }) ->
-	{ reply, ok, Room };
+handle_call( bans, _From, Room = #room{ bans = Bans }) ->
+	{ reply, { ok, Bans }, Room };
 
-handle_call( close, _From, Room ) ->
-	{ reply, ok, Room };
+handle_call( peers, _From, Room = #room{ peers = Peers }) ->
+	{ reply, { ok, Peers }, Room };
+
+handle_call( close, _From, Room = #room{ socket = Socket }) ->
+	gen_udp:close(Socket),
+	{ reply, { stop, ok }, Room };
 
 handle_call( _Message, _From, Room ) ->
 	{ reply, ok, Room }.
@@ -88,10 +115,24 @@ handle_call( _Message, _From, Room ) ->
 handle_cast( _Message, Room ) ->
 	{ noreply, Room }.
 
-handle_info( _Message, Room ) ->
+handle_info({ udp, _Client, IPAddr, Port, Packet }, Room = #room{ users = Users, bans = Bans, socket = Socket }) ->
+	case member(IPAddr,Port,Bans) of
+		true -> io:format("[Banned] ~p:~p ~p~n", [ IPAddr,Port,Packet]);
+		false -> false
+	end,
+	case member(IPAddr,Port,Users) of
+		true -> 
+			io:format("[Relay] ~p:~p ~p~n", [ IPAddr,Port,Packet]),
+			[ broadcast(Socket,U,Packet) || U <- except(IPAddr,Port,Users) ];
+		false -> false
+	end,
+	{ noreply, Room };
+
+handle_info( Message, Room ) ->
+	io:format("Got message ~p~n", [ Message ]),
 	{ noreply, Room }.
 
-terminate(Reason, Room = #room{ name = Name } ) ->
+terminate(Reason, #room{ name = Name } ) ->
 	io:format("Shutting down ~p because ~p~n", [ Name, Reason ]),
 	ok.
 
@@ -99,3 +140,16 @@ code_change( _Old, Room, _Extra) ->
 	{ ok, Room }.	
 
 
+broadcast(Socket, #user{ ipaddr = IPAddr, port = Port }, Message ) ->
+	gen_udp:send(Socket,IPAddr,Port,Message).
+
+relay(Socket, #peer{ ipaddr = IPAddr, port = Port }, Message ) ->
+	gen_udp:send(Socket,IPAddr,Port,Message).
+
+member(IPAddr,Port,Users) ->
+	[] /= lists:filtermap( fun(#user{ ipaddr = I, port = P }) -> 
+		(I =:= IPAddr) and (P =:= Port) end, Users).
+	
+except(IPAddr,Port,Users) ->
+	lists:filtermap( fun(#user{ ipaddr = I, port = P }) ->
+		(I /= IPAddr) or (P /= Port) end, Users ).
